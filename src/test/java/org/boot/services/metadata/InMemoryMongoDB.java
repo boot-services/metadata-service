14d274dd23d7f2832696a0cb9e64f3e00537cc12
sunitparekh <parekh.sunit@gmail.com>
2018-05-01 10:09:35 +0530

    removed security startup

---
+++ src/test/java/org/boot/services/metadata/InMemoryMongoDB.java
@@ -0,0 +1,20 @@
+package org.boot.services.metadata;
+
+import com.github.fakemongo.Fongo;
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+@Configuration
+public class InMemoryMongoDB extends AbstractMongoConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "InMemoryMongo";
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        return new Fongo(getDatabaseName()).getMongo();
+    }
+}
